8ee6b6b1a6ca2f634d48fa3075ba1af5450119aa
sumantagethub <janasumanta721152@gmail.com>
2026-08-16 19:28:16 +0530

    newrg

--- terraform_resualble_module_01_08_27/environements/variable.tf
+++ terraform_resualble_module_01_08_27/environements/variable.tf
@@ -1,9 +1,9 @@
-variable "dev-rg"{}
+variable "dev-rg" {}
 variable "dev-vnet" {}
-variable "dev-subnet"{}
-variable "dev-nic"{}
-variable "dev-vm"{}
+variable "dev-subnet" {}
+variable "dev-nic" {}
+variable "dev-vm" {}
 variable "dev-bastion-host" {}
-variable "dev-pip"{}
+variable "dev-pip" {}
 variable "nsgs" {}
-variable "stgs"{}
+variable "stgs" {}
